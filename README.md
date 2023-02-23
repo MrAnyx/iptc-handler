4f37eb0761a8a5bad09a823227456649ac0febad
MrAnyx <1ptitigre@gmail.com>
2023-02-23 12:18:18 +0000

    feat(auth) Added user entity with login system

--- README.md
+++ README.md
@@ -6,7 +6,11 @@ docker build -t iptc-handler .
 
 ```bash
 docker run -p 8088:80 -tid iptc-handler
+php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration --quiet
+php bin/console doctrine:fixtures:load --no-interaction --quiet
 ```
+
+
 
 Then visit `localhost:8088`
 
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setUsername("user");
+        $user->setPassword($this->passwordHasher->hashPassword($user, "password"));
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
